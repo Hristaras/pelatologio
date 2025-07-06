9bf4eb2f73d1ea2eb399ac97e4aeb0ab80dc6613
Hristaras <kalasxristoss@gmail.com>
2025-07-06 09:45:20 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,7 @@
-function addClient() {
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  const debt = document.getElementById("debt").value;
-  const phone = document.getElementById("phone").value;
-
-  db.collection("clients").add({
-    firstName,
-    lastName,
-    debt,
-    phone
-  });
-
-  document.getElementById("firstName").value = "";
-  document.getElementById("lastName").value = "";
-  document.getElementById("debt").value = "";
-  document.getElementById("phone").value = "";
-}
-
 function deleteClient(id) {
   const confirmDelete = confirm("Θέλεις σίγουρα να διαγράψεις αυτόν τον πελάτη;");
   if (confirmDelete) {
     db.collection("clients").doc(id).delete();
-  }
-}
-
-function editClient(id, currentData) {
-  const newFirstName = prompt("Νέο όνομα:", currentData.firstName);
-  const newLastName = prompt("Νέο επίθετο:", currentData.lastName);
-  const newDebt = prompt("Νέα χρωστούμενα:", currentData.debt);
-  const newPhone = prompt("Νέο τηλέφωνο:", currentData.phone);
-
-  if (newFirstName && newLastName && newDebt && newPhone) {
-    db.collection("clients").doc(id).update({
-      firstName: newFirstName,
-      lastName: newLastName,
-      debt: newDebt,
-      phone: newPhone
-    });
   }
 }
 
@@ -46,12 +11,7 @@ db.collection("clients").onSnapshot(snapshot => {
   snapshot.forEach(doc => {
     const client = doc.data();
     const li = document.createElement("li");
-    li.textContent = `${client.firstName} ${client.lastName} - Χρωστάει: €${client.debt} - Τηλ: ${client.phone} `;
-
-    const editBtn = document.createElement("button");
-    editBtn.textContent = "Επεξεργασία";
-    editBtn.onclick = () => editClient(doc.id, client);
-    li.appendChild(editBtn);
+    li.textContent = `${client.firstName} ${client.lastName} - Χρωστάει: €${client.debt} - Τηλ: ${client.phone}`;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.textContent = "Διαγραφή";
