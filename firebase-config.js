4ee84f92f40f2f9a824e3a87299e4ab8deb05b5e
Hristaras <kalasxristoss@gmail.com>
2025-07-06 10:00:11 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAqe4HFF63ONdXsG7iMPCqFMG5IY8C19qw",
   authDomain: "pelatologio-864f4.firebaseapp.com",
   projectId: "pelatologio-864f4",
-  storageBucket: "pelatologio-864f4.firebasestorage.app",
+  storageBucket: "pelatologio-864f4.appspot.com",
   messagingSenderId: "1082547737442",
   appId: "1:1082547737442:web:6a7a30bd3f8185c814d23b"
 };
