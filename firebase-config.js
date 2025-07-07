45527651a3f463efcbe92db910a0081d76bda2e1
Hristaras <kalasxristoss@gmail.com>
2025-07-07 10:31:02 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAqe4HFF63ONdXsG7iMPCqFMG5IY8C19qw",
   authDomain: "pelatologio-864f4.firebaseapp.com",
-  databaseURL: "https://pelatologio-864f4-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "pelatologio-864f4",
-  storageBucket: "pelatologio-864f4.firebasestorage.app",
+  storageBucket: "pelatologio-864f4.appspot.com",
   messagingSenderId: "1082547737442",
   appId: "1:1082547737442:web:6a7a30bd3f8185c814d23b"
 };
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-let editingClientId = null;
+let currentUserId = null;
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    currentUserId = user.uid;
+    document.getElementById('auth').style.display = 'none';
+    document.getElementById('app').style.display = 'block';
+    loadClients();
+  } else {
+    document.getElementById('auth').style.display = 'block';
+    document.getElementById('app').style.display = 'none';
+  }
+});
+
+function signInWithGoogle() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+    .catch(error => alert(error.message));
+}
+
+function signOut() {
+  firebase.auth().signOut();
+}
 
 function addClient() {
   const firstName = document.getElementById('firstName').value;
@@ -7,6 +29,7 @@ function addClient() {
   const phone = document.getElementById('phone').value;
 
   db.collection("clients").add({
+    userId: currentUserId,
     firstName,
     lastName,
     debt,
@@ -27,56 +50,13 @@ function clearInputs() {
 function loadClients() {
   const list = document.getElementById('clientList');
   list.innerHTML = '';
-  db.collection("clients").get().then(snapshot => {
+  db.collection("clients").where("userId", "==", currentUserId).get().then(snapshot => {
     snapshot.forEach(doc => {
       const client = doc.data();
       const li = document.createElement('li');
-      li.innerHTML = `
-        <strong>${client.firstName} ${client.lastName}</strong><br>
-        Χρωστάει: €${client.debt} | Τηλέφωνο: ${client.phone}
-        <br>
-        <button onclick="editClient('${doc.id}', '${client.firstName}', '${client.lastName}', '${client.debt}', '${client.phone}')">Επεξεργασία</button>
-        <button onclick="confirmDelete('${doc.id}')">Διαγραφή</button>
-      `;
+      li.innerHTML = `<strong>${client.firstName} ${client.lastName}</strong><br>
+        Χρωστάει: €${client.debt} | Τηλέφωνο: ${client.phone}`;
       list.appendChild(li);
     });
   });
 }
-
-function editClient(id, firstName, lastName, debt, phone) {
-  editingClientId = id;
-  document.getElementById('editForm').style.display = 'block';
-  document.getElementById('editFirstName').value = firstName;
-  document.getElementById('editLastName').value = lastName;
-  document.getElementById('editDebt').value = debt;
-  document.getElementById('editPhone').value = phone;
-}
-
-function saveEdit() {
-  const updatedData = {
-    firstName: document.getElementById('editFirstName').value,
-    lastName: document.getElementById('editLastName').value,
-    debt: document.getElementById('editDebt').value,
-    phone: document.getElementById('editPhone').value
-  };
-
-  db.collection("clients").doc(editingClientId).update(updatedData).then(() => {
-    document.getElementById('editForm').style.display = 'none';
-    loadClients();
-  });
-}
-
-function cancelEdit() {
-  document.getElementById('editForm').style.display = 'none';
-  editingClientId = null;
-}
-
-function confirmDelete(id) {
-  if (confirm("Θέλεις σίγουρα να διαγράψεις αυτόν τον πελάτη;")) {
-    db.collection("clients").doc(id).delete().then(() => {
-      loadClients();
-    });
-  }
-}
-
-window.onload = loadClients;
