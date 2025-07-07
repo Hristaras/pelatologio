bccd97b73751948c8f096a9c19a287a833edf86b
Hristaras <kalasxristoss@gmail.com>
2025-07-07 10:43:44 +0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,12 @@
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAqe4HFF63ONdXsG7iMPCqFMG5IY8C19qw",
-  authDomain: "pelatologio-864f4.firebaseapp.com",
-  projectId: "pelatologio-864f4",
-  storageBucket: "pelatologio-864f4.appspot.com",
-  messagingSenderId: "1082547737442",
-  appId: "1:1082547737442:web:6a7a30bd3f8185c814d23b"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
--- main.js
+++ main.js
@@ -1,62 +1,51 @@
-let currentUserId = null;
 
-firebase.auth().onAuthStateChanged(user => {
-  if (user) {
-    currentUserId = user.uid;
-    document.getElementById('auth').style.display = 'none';
-    document.getElementById('app').style.display = 'block';
-    loadClients();
-  } else {
-    document.getElementById('auth').style.display = 'block';
-    document.getElementById('app').style.display = 'none';
-  }
-});
-
-function signInWithGoogle() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-    .catch(error => alert(error.message));
-}
-
-function signOut() {
-  firebase.auth().signOut();
-}
+const db = firebase.database().ref("clients");
 
 function addClient() {
-  const firstName = document.getElementById('firstName').value;
-  const lastName = document.getElementById('lastName').value;
-  const debt = document.getElementById('debt').value;
-  const phone = document.getElementById('phone').value;
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const debt = document.getElementById("debt").value;
+  const phone = document.getElementById("phone").value;
 
-  db.collection("clients").add({
-    userId: currentUserId,
+  db.push({
     firstName,
     lastName,
     debt,
     phone
-  }).then(() => {
-    clearInputs();
-    loadClients();
   });
+
+  clearInputs();
 }
 
 function clearInputs() {
-  document.getElementById('firstName').value = '';
-  document.getElementById('lastName').value = '';
-  document.getElementById('debt').value = '';
-  document.getElementById('phone').value = '';
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("debt").value = "";
+  document.getElementById("phone").value = "";
 }
 
 function loadClients() {
-  const list = document.getElementById('clientList');
-  list.innerHTML = '';
-  db.collection("clients").where("userId", "==", currentUserId).get().then(snapshot => {
-    snapshot.forEach(doc => {
-      const client = doc.data();
-      const li = document.createElement('li');
-      li.innerHTML = `<strong>${client.firstName} ${client.lastName}</strong><br>
-        Χρωστάει: €${client.debt} | Τηλέφωνο: ${client.phone}`;
+  const list = document.getElementById("clientList");
+  db.on("value", (snapshot) => {
+    list.innerHTML = "";
+    snapshot.forEach((child) => {
+      const client = child.val();
+      const li = document.createElement("li");
+      li.innerHTML = `
+        <strong>${client.firstName} ${client.lastName}</strong><br />
+        Χρωστάει: €${client.debt} | Τηλέφωνο: ${client.phone}
+        <br />
+        <button onclick="confirmDelete('${child.key}')">Διαγραφή</button>
+      `;
       list.appendChild(li);
     });
   });
 }
+
+function confirmDelete(id) {
+  if (confirm("Θέλεις σίγουρα να διαγράψεις αυτόν τον πελάτη;")) {
+    db.child(id).remove();
+  }
+}
+
+window.onload = loadClients;
